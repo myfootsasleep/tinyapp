cea90e81beb6e346cd43f09068918b650c890fa1
myfootsasleep <richardoda@gmail.com>
2023-05-02 23:44:04 -0700

    addd urls_new.ejs, template for the add new url page added a random short URL generator, and added app.post for retrieve the url from said page

--- express_server.js
+++ express_server.js
@@ -3,6 +3,17 @@ const app = express();
 const PORT = 8080;
 
 app.set("view engine", "ejs");
+app.use(express.urlencoded({extnded: true}));
+
+const generateShortUrl = () => {
+  let result = '';
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  const charactersLength = characters.length;
+  for (let i = 0; i < 6; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+};
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighouselabs.ca",
@@ -12,6 +23,11 @@ const urlDatabase = {
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
+});
+
+app.post("/urls", (req,res) => {
+  console.log(req.body);
+  res.send("Ok");
 });
 
 for (const amount in urlDatabase) {
@@ -36,5 +52,9 @@ app.get("/urls.json", (req,res) => {
 
 app.get('/hello',(req,res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
+
+app.get("/urls/new", (req,res) => {
+  res.render("urls_new");
 });
 
