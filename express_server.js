11789cc0eb17469ad6286692d8c7d637fd6c5a9f
myfootsasleep <richardoda@gmail.com>
2023-05-07 21:05:14 -0700

    same issue as previous, with the addition of new objects, header now displays logged in by an email, however, the website does not function properly, will go back to fix when all extra functions are added. As of now I have created a register.ejs where one can enter an email and password, in the express_server.js I addd checks to see if the field is blank or if the email already exists.

--- express_server.js
+++ express_server.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = 8080;
 
 app.set("view engine", "ejs");
-app.use(express.urlencoded({extnded: true}));
+app.use(express.urlencoded({extended: true}));
 app.use(cookieParser());
 
 const generateShortUrl = () => {
@@ -16,56 +16,120 @@ const generateShortUrl = () => {
   }
   return result;
 };
+let users = {
+
+};
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9ssm5xk": "http://www.google.com"
 };
 
+const getUserByEmail = (email) => {
+  return Object.values(users).find((user) => user.email === email);
+};
 
-app.use((req, res, next) => {
-  const username = req.cookies.username; // Assuming you are using cookies to store the username
-  res.locals.username = username; // Set the username in res.locals
-  next();
+app.post("/register", (req, res) => {
+  const { email, password } = req.body;
+
+  // Check if email or password are empty strings
+  if (!email || !password) {
+    res.status(400).send("Email and password cannot be empty");
+    return;
+  }
+
+  // Check if email already exists in users object
+  const existingUser = getUserByEmail(email);
+  if (existingUser) {
+    res.status(400).send("Email already registered");
+    return;
+  }
+
+  // Proceed with user registration
+  const userRandomID = generateShortUrl();
+  const newUser = {
+    userId: userRandomID,
+    email: email,
+    password: password
+  };
+  users[userRandomID] = newUser;
+
+  res.cookie("user_id", userRandomID);
+  res.redirect("/urls");
 });
+app.get("/register", (req, res) => {
+  const templateVars = {
+    users: null, // Set a default value for the user variable
+    // ... other variables ...
+  };
+
+  res.render("register", templateVars);
+});
+
+// app.use((req, res, next) => {
+//   const username = req.cookies.username; // Assuming you are using cookies to store the username
+//   res.locals.username = username; // Set the username in res.locals
+//   next();
+// });
 //Define route for handling new URL submissions
 app.post("/urls", (req,res) => {
   const shortURL = generateShortUrl();
   urlDatabase[shortURL] = req.body.longURL;
   res.redirect(`/urls/${shortURL}`);
 });
-
+// app.post("/login", (req,res) => {
+//   const username = req.body.username;
+//   res.cookie("username", username);
+//   res.redirect("/urls");
+// });
+//Define route for when someone presses the logout button
+app.post("/logout", (req,res) => {
+  res.clearCookie("user_id");
+  res.redirect("/urls");
+});
 //Define route for showing the index page with all existing URLs - urls_index.ejs
 app.get("/urls", (req, res) => {
+  const userId = req.cookies.user_id;
+  const user = users[userId];
   const templateVars = {
-    username: req.cookies.username,
-    urls: urlDatabase };
+    user: user,
+    urls: urlDatabase,
+  };
   res.render("urls_index", templateVars);
 });
 
 //Define route for showing the form to submit a new URL - url_new.ejs
-app.get("/urls/new", (req,res) => {
+app.get("/urls/new", (req, res) => {
+  const userId = req.cookies.user_id;
+  const user = users[userId];
   const templateVars = {
+    user: user,
   };
   res.render("urls_new", templateVars);
 });
 
 //Define route for handling requests of newly added URL
 app.get("/urls/:id", (req, res) => {
+  const userId = req.cookies.user_id;
+  const user = users[userId];
   const templateVars = {
     urls: urlDatabase,
     longURL: urlDatabase[req.params.id],
-    id: req.params.id
+    id: req.params.id,
+    user: user
   };
   res.render("urls_show", templateVars);
 });
 
 //Define route for handling the edit button press
-app.post("/urls/:id", (req, res) => {
+app.post("/urls/:id/edit", (req, res) => {
+  const userId = req.cookies.user_id;
+  const user = users[userId];
   const templateVars = {
     urls: urlDatabase,
     longURL: urlDatabase[req.params.id],
-    id: req.params.id
+    id: req.params.id,
+    user: user
   };
   res.render("urls_show", templateVars);
 });
@@ -92,20 +156,9 @@ app.get("/u/:id", (req, res) => {
 });
 
 //Define route for when someone enters username and presses login
-app.post("/login", (req,res) => {
-  const username = req.body.username;
-  res.cookie("username", username);
-  res.redirect("/urls");
-});
-//Define route for when someone presses the logout button
-app.post("/logout", (req,res) => {
-  res.clearCookie("username");
-  res.redirect("/urls");
-});
 
-app.get("/register", (req, res) => {
-  res.render("register");
-});
+
+
 //Define route for returning JSON object of the urlDatabase
 app.get("/urls.json", (req,res) => {
   res.json(urlDatabase);
