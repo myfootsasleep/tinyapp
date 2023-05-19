5ccb321a707ed3900a9bf2273c7454c8f23610f1
myfootsasleep <richardoda@gmail.com>
2023-05-18 23:59:15 -0700

    moved all the functions into the helper.js file, I also moved all the database variables into a new database.js file. Added module.exports and requires, as needed

--- express_server.js
+++ express_server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cookieSession = require("cookie-session");
-const {getUserByEmail} = require("./helper.js");
+const {getUserByEmail, generateShortUrl, checkShortUrl, urlsForUser} = require("./helper.js");
+const {users, urlDatabase} = require("./database.js");
 const app = express();
 const PORT = 8080;
 const bcrypt = require("bcryptjs");
@@ -10,50 +11,6 @@ app.use(cookieSession({
   name: 'session',
   keys: ["sefdaveaf"],
 }));
-
-//Function that create a unique Id used in both users and shortURLs
-const generateShortUrl = () => {
-  let result = '';
-  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-  const charactersLength = characters.length;
-  for (let i = 0; i < 6; i++) {
-    result += characters.charAt(Math.floor(Math.random() * charactersLength));
-  }
-  return result;
-};
-
-//Function that checks to see if a shortUrl Exists or not
-const checkShortUrl = (shortURL) => {
-  return Object.keys(urlDatabase).find((shortendURL) => shortendURL === shortURL);
-};
-
-//Function that returns URLs where the userID is equal to the id of the currently logged-in user.
-const urlsForUser = (id) => {
-  return Object.entries(urlDatabase).reduce((userUrls, [key, value]) => {
-    if (value.userID === id) {
-      userUrls[key] = value;
-    }
-    return userUrls;
-  }, {});
-};
-
-//Object of users
-let users = {
-  //Test user: password = test
-  'aJ48lW': { userId: 'aJ48lW', email: 'richardoda@gmail.com', password: '$2a$10$HHoNnJZma5YLrMCwLo40t.vmLNwbetEdxCRpGpCUyxpDDZY8KEu2m' }
-};
-
-//Object of urlDatabase
-const urlDatabase = {
-  b6UTxQ: {
-    longURL: "https://www.tsn.ca",
-    userID: "aJ48lW",
-  },
-  i3BoGr: {
-    longURL: "https://www.google.ca",
-    userID: "aJ48lW",
-  },
-};
 
 //Define route for login page
 app.get("/login", (req, res) => {
--- helper.js
+++ helper.js
@@ -1,3 +1,5 @@
+const {users, urlDatabase} = require("./database");
+
 //Function that checks to see if the email is already registered or not
 const getUserByEmail = (email, db) => {
   // loop in database keys
@@ -10,5 +12,31 @@ const getUserByEmail = (email, db) => {
   return undefined;
 };
 
+//Function that create a unique Id used in both users and shortURLs
+const generateShortUrl = () => {
+  let result = '';
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  const charactersLength = characters.length;
+  for (let i = 0; i < 6; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+};
 
-module.exports = {getUserByEmail};
+//Function that checks to see if a shortUrl Exists or not
+const checkShortUrl = (shortURL) => {
+  return Object.keys(urlDatabase).find((shortendURL) => shortendURL === shortURL);
+};
+
+//Function that returns URLs where the userID is equal to the id of the currently logged-in user.
+const urlsForUser = (id) => {
+  return Object.entries(urlDatabase).reduce((userUrls, [key, value]) => {
+    if (value.userID === id) {
+      userUrls[key] = value;
+    }
+    return userUrls;
+  }, {});
+};
+
+
+module.exports = {getUserByEmail, generateShortUrl, checkShortUrl, urlsForUser};
