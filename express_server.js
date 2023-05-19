f6cf2c324f33a726e22daffd86d3edf519d370d3
myfootsasleep <richardoda@gmail.com>
2023-05-18 21:51:18 -0700

    rearranged my code so get comes first then post and then after that listen

--- express_server.js
+++ express_server.js
@@ -36,11 +36,13 @@ const urlsForUser = (id) => {
     return userUrls;
   }, {});
 };
+
 //Object of users
 let users = {
   //Test user: password = test
   'aJ48lW': { userId: 'aJ48lW', email: 'richardoda@gmail.com', password: '$2a$10$HHoNnJZma5YLrMCwLo40t.vmLNwbetEdxCRpGpCUyxpDDZY8KEu2m' }
 };
+
 //Object of urlDatabase
 const urlDatabase = {
   b6UTxQ: {
@@ -52,6 +54,25 @@ const urlDatabase = {
     userID: "aJ48lW",
   },
 };
+
+//Define route for login page
+app.get("/login", (req, res) => {
+  const userId = req.session.user_id;
+  const user = users[userId];
+  const templateVars = {
+    user: user,
+    urls: urlDatabase,
+  };
+  if (user) {
+    res.redirect("/urls");
+  }
+  res.render("login", templateVars);
+});
+
+//Define a basic "hello world" route in root
+app.get("/", (req, res) => {
+  res.redirect("/login");
+});
 
 // Define route for /register path essentially calls the /register page
 app.get("/register",(req,res) => {
@@ -67,92 +88,7 @@ app.get("/register",(req,res) => {
     res.render("register", templateVars);
   }
 });
-//Define route for handling new URL submissions
-app.post("/urls", (req,res) => {
-  const userId = req.session.user_id;
-  const user = users[userId];
-  if (!user) {
-    res.status(400).send("You can't shorten URLs unless you are a user, please login or register!");
-  } else {
-    const shortURL = generateShortUrl();
-    urlDatabase[shortURL] = {
-      longURL: req.body.longURL,
-      userID: userId
-    };
-    res.redirect(`/urls/${shortURL}`);
-  }
-});
 
-//Define route pate for when someone creates a new account
-app.post("/register/createAccount", (req, res) => {
-  const { email, password } = req.body;
-
-  // Check if email or password are empty strings
-  if (!email || !password) {
-    res.status(400).send("Email and password cannot be empty");
-    return;
-  }
-
-  // Check if email already exists in users object
-  const existingUser = getUserByEmail(email,users);
-  if (existingUser) {
-    res.status(400).send("Email already registered");
-    return;
-  } else {
-
-    // Proceed with user registration
-    const userRandomID = generateShortUrl();
-    const hashedPass = bcrypt.hashSync(password, 10);
-    const newUser = {
-      userId: userRandomID,
-      email: email,
-      password: hashedPass,
-    };
-    req.session.user_id = newUser.userId;
-    users[userRandomID] = newUser;
-    res.redirect("/urls");
-  }
-});
-
-
-//Define route for login page
-app.get("/login", (req, res) => {
-  const userId = req.session.user_id;
-  const user = users[userId];
-  const templateVars = {
-    user: user,
-    urls: urlDatabase,
-  };
-  if (user) {
-    res.redirect("/urls");
-  }
-  res.render("login", templateVars);
-});
-//Define route for when someone enters username and presses login
-app.post("/login/verify", (req, res) => {
-  const { email, password } = req.body;
-  //const hashedPass = bcrypt.hashSync(password, 10);
-  // Check if email or password are empty strings
-  if (!email || !password) {
-    res.status(400).send("Email and password cannot be empty");
-    return;
-  }
-
-  // Find the user by email
-  const user = Object.values(users).find((user) => user.email === email);
-  // Check if user exists and if the password matches
-  if (user && bcrypt.compareSync(password, user["password"])) {
-    req.session.user_id = user.userId;
-    res.redirect("/urls");
-  } else {
-    res.status(401).send("Invalid email or password");
-  }
-});
-//Define route for when someone presses the logout button
-app.post("/logout", (req,res) => {
-  req.session.user_id = null;
-  res.redirect("/login");
-});
 //Define route for showing the index page with all existing URLs - urls_index.ejs
 app.get("/urls", (req, res) => {
   const userId = req.session.user_id;
@@ -214,6 +150,72 @@ app.get("/urls/:id/edit", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
+//Define route for returning to full website once clicked
+app.get("/u/:id", (req, res) => {
+  const shortURL = req.params.id;
+  const existingURL = checkShortUrl(shortURL);
+  if (existingURL) {
+    const longURL = urlDatabase[req.params.id].longURL;
+    res.redirect(longURL);
+  } else {
+    res.status(400).send("Trying to TinyLink that doesn't exist");
+  }
+});
+
+//Define route pate for when someone creates a new account
+app.post("/register/createAccount", (req, res) => {
+  const { email, password } = req.body;
+  // Check if email or password are empty strings
+  if (!email || !password) {
+    res.status(400).send("Email and password cannot be empty");
+    return;
+  }
+  // Check if email already exists in users object
+  const existingUser = getUserByEmail(email,users);
+  if (existingUser) {
+    res.status(400).send("Email already registered");
+    return;
+  } else {
+    // Proceed with user registration
+    const userRandomID = generateShortUrl();
+    const hashedPass = bcrypt.hashSync(password, 10);
+    const newUser = {
+      userId: userRandomID,
+      email: email,
+      password: hashedPass,
+    };
+    req.session.user_id = newUser.userId;
+    users[userRandomID] = newUser;
+    res.redirect("/urls");
+  }
+});
+
+//Define route for when someone enters username and presses login
+app.post("/login/verify", (req, res) => {
+  const { email, password } = req.body;
+  //const hashedPass = bcrypt.hashSync(password, 10);
+  // Check if email or password are empty strings
+  if (!email || !password) {
+    res.status(400).send("Email and password cannot be empty");
+    return;
+  }
+  // Find the user by email
+  const user = Object.values(users).find((user) => user.email === email);
+  // Check if user exists and if the password matches
+  if (user && bcrypt.compareSync(password, user["password"])) {
+    req.session.user_id = user.userId;
+    res.redirect("/urls");
+  } else {
+    res.status(401).send("Invalid email or password");
+  }
+});
+
+//Define route for when someone presses the logout button
+app.post("/logout", (req,res) => {
+  req.session.user_id = null;
+  res.redirect("/login");
+});
+
 //Define route for updating long URLS
 app.post("/urls/:id/update", (req, res) =>{
   const id = req.params.id;
@@ -235,22 +237,22 @@ app.post("/urls/:id/delete", (req, res) => {
   res.redirect("/urls");
 });
 
-//Define route for returning to full website once clicked
-app.get("/u/:id", (req, res) => {
-  const shortURL = req.params.id;
-  const existingURL = checkShortUrl(shortURL);
-  if (existingURL) {
-    const longURL = urlDatabase[req.params.id].longURL;
-    res.redirect(longURL);
+//Define route for handling new URL submissions
+app.post("/urls", (req,res) => {
+  const userId = req.session.user_id;
+  const user = users[userId];
+  if (!user) {
+    res.status(400).send("You can't shorten URLs unless you are a user, please login or register!");
   } else {
-    res.status(400).send("Trying to TinyLink that doesn't exist");
+    const shortURL = generateShortUrl();
+    urlDatabase[shortURL] = {
+      longURL: req.body.longURL,
+      userID: userId
+    };
+    res.redirect(`/urls/${shortURL}`);
   }
 });
 
-//Define a basic "hello world" route in root
-app.get("/", (req, res) => {
-  res.redirect("/login");
-});
 //Start the server
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
